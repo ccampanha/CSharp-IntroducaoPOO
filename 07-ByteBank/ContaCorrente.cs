241dc120f2f3add8088250180ed4ab27c8c9c608
Cristiana Campanha <ccampanha@hotmail.com>
2022-02-07 23:51:12 -0300

    construtor e membros estáticos

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,83 @@
+﻿namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+
+        private int _agencia;
+        private int _numero { get; set; }
+        
+        private double _saldo = 100;
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+        }
+        public int Agencia
+        {
+            get { return _agencia; }
+            set 
+            {
+                if (value < 0)
+                    return;
+                _agencia = value; 
+            }
+        }
+
+        public int Numero 
+        { 
+            get { return _numero; }
+            set
+            {
+                if (value < 0)
+                    return;
+                _numero = value;
+            }    
+        
+        }
+        public double Saldo
+        {
+            get { return _saldo; }
+
+            set
+            {
+                if (value < 0)
+                    return;
+                _saldo = value;
+     
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente contaDaGabi = new ContaCorrente(867, 86712541);
+            Console.WriteLine(contaDaGabi.Agencia);
+            Console.WriteLine(contaDaGabi.Numero);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.ReadLine();
+
+        }
+    }
+}
